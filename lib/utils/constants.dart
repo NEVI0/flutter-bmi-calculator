35ff02168b16c6b8ec25b4463257ab1e40e46efe
NEVI0 <neviocostamagagnin@gmail.com>
2023-05-13 16:24:24 -0300

    feat: adding constants file

---
+++ lib/utils/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+const Color kActiveCardColor = Color(0xFF1D1E33);
+const Color kInactiveCardColor = Color(0xFF111328);
+const Color kMainColor = Color(0xFFEB1555);
+const Color kTextColor = Color(0xFF8D8E98);
+
+const TextStyle kTextStyle = TextStyle(
+	fontSize: 16,
+	color: Color(0xFF8D8E98)
+);
+
+const TextStyle kBoldTextStyle = TextStyle(
+	fontSize: 48,
+	fontWeight: FontWeight.w900
+);
+
+const double kMinHeightValue = 120.0;
+const double kMaxHeightValue = 220.0;
